93153a66149ee6fb9550100d66d87c57416ad03a
javierschwendener <sch18534@uvg.edu.gt>
2022-08-09 19:47:54 -0600

    Pila Anidada ATmega328P en C
    Se creó el proyecto y se completó el código de la pila anidada en C para el ATmega328P

---
+++ C/ATMega/PilaAnidada/main.c
@@ -0,0 +1,91 @@
+/*
+////////////////
+ PILA ANIDADA C
+////////////////
+ */ 
+
+#include <xc.h>
+
+/*PROTOTIPOS*/
+void func1(void);
+void func2(void);
+void func3(void);
+void func4(void);
+void func5(void);
+void func6(void);
+void func7(void);
+void func8(void);
+
+/*VARIABLES*/
+
+
+void setup(void){
+	// Configuracion inicial de los puertos como salidas
+	DDRB = 0b11111111;
+	DDRC = 0b11111111;
+	DDRD = 0b11111111;
+	// Se establece el valor de los puertos como 0
+	PORTB = 0;
+	PORTC = 0;
+	PORTD = 0;
+}
+
+int main(void)
+{
+	// Se ejecuta la configuracion de los puertos
+	setup();
+    while(1)
+    {
+		// Inicio de la medicion
+		PORTD0 >= 1;
+		func1();
+		// Fin de la medicion
+		PORTD = 0;
+    }
+}
+
+void func1(void){
+	asm("nop");
+	func2();
+	return;
+}
+
+void func2(void){
+	asm("nop");
+	func3();
+	return;
+}
+
+void func3(void){
+	asm("nop");
+	func4();
+	return;
+}
+
+void func4(void){
+	asm("nop");
+	func5();
+	return;
+}
+
+void func5(void){
+	asm("nop");
+	func6();
+	return;
+}
+
+void func6(void){
+	asm("nop");
+	func7();
+	return;
+}
+
+void func7(void){
+	asm("nop");
+	func8();
+	return;
+}
+void func8(void){
+	PORTD1 >= 1;
+	return;
+}
