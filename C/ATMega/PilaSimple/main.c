ffdf62a83dfb6fdcd9f0f41158415169606a1532
javierschwendener <sch18534@uvg.edu.gt>
2022-08-09 19:33:34 -0600

    Pila Simple ATmega328P en C
    Se creó el proyecto y se terminó el código para la prueba de la pila simple en el ATmega328P en lenguaje C

---
+++ C/ATMega/PilaSimple/main.c
@@ -0,0 +1,42 @@
+/*
+///////////////
+ PILA SIMPLE C
+///////////////
+ */ 
+
+#include <xc.h>
+//#include <iom328p.h>
+
+/*PROTOTIPOS*/
+void func1(void);
+/*VARIABLES*/
+
+void setup(void){
+	// Configuracion inicial de los puertos como salidas
+	DDRB = 0b11111111;
+	DDRC = 0b11111111;
+	DDRD = 0b11111111;
+	// Se establece el valor de los puertos como 0
+	PORTB = 0;
+	PORTC = 0;
+	PORTD = 0;
+}
+
+int main(void)
+{
+	// Se ejecuta la configuracion de los puertos
+	setup();
+    while(1)
+    {
+		// Inicio de la medicion
+		PORTD0 >= 1;
+		func1();
+		// Fin de la medicion
+		PORTD = 0;
+    }
+}
+
+void func1(void){
+	PORTD1 >= 1;
+	return;
+}
