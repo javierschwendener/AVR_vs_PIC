79f236b3dce2ca020aa4845fda59fc8e2455b11f
javierschwendener <sch18534@uvg.edu.gt>
2022-09-06 20:03:29 -0600

    Temporizador sin interrupciones ATmega C
    Se completó el código para el temporizador sin interrupciones del ATmega en C

--- C/ATMega/TemporizadorSinInterrupciones/main.c
+++ C/ATMega/TemporizadorSinInterrupciones/main.c
@@ -1,16 +1,53 @@
 /*
- * main.c
- *
- * Created: 9/6/2022 7:27:11 PM
- *  Author: Schinzii
+///////////////////////////////////
+ TEMPROIZADOR SIN INTERRUPCIONES C
+///////////////////////////////////
  */ 
 
 #include <xc.h>
 
+/*PROTOTIPOS*/
+void timer0_set(void);
+
+/*VARIABLES*/
+int counter = 0;
+
+void setup(void){
+	// Puertos B y C como salidas
+	DDRB = 0b11111111;
+	DDRC = 0b11111111;
+	// Puerto D como entrada
+	DDRD = 0b00000000;
+	// Se establece el valor de los puertos como 0
+	PORTB = 0;
+	PORTC = 0;
+	PORTD = 0;
+}
+
 int main(void)
 {
-    while(1)
-    {
-        //TODO:: Please write your application code 
-    }
+	// Se ejecuta la configuracion de los puertos
+	setup();
+	timer0_set();
+    while(1){
+		if(TCNT0 == 128){
+			TCNT0 = 0;
+			counter = counter + 1;
+		}
+		if(counter == 15){
+			counter = 0;
+			if(PINB & (1<<0)){
+				PORTB = 0;
+			}
+			else{
+				PORTB = 1;
+			}
+		}
+	}
+}
+
+void timer0_set(void){
+	TCCR0A = 0B00000000;
+	TCCR0B = 0B00000101;
+	TCNT0 = 0;
 }
