72fd05c4955533e33072600c4cca625c6c38af66
javierschwendener <sch18534@uvg.edu.gt>
2022-09-06 19:30:37 -0600

    ATmega328p temporizador sin interrupciones C
    Se creó el proyecto para el temporizador sin interrupciones para el ATmega en C

---
+++ C/ATMega/TemporizadorSinInterrupciones/main.c
@@ -0,0 +1,16 @@
+/*
+ * main.c
+ *
+ * Created: 9/6/2022 7:27:11 PM
+ *  Author: Schinzii
+ */ 
+
+#include <xc.h>
+
+int main(void)
+{
+    while(1)
+    {
+        //TODO:: Please write your application code 
+    }
+}
