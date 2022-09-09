cadc6eb20ec38886954c7635392e07395dda3e9f
javierschwendener <sch18534@uvg.edu.gt>
2022-09-08 20:24:01 -0600

    Temporizador con interrupciones ATmega C
    Se creó el proyecto y completó el código del temporizador con interrupciones en C para el ATmega

---
+++ C/ATMega/TemporizadorConInterrupciones/main.c
@@ -0,0 +1,60 @@
+/*
+///////////////////////////////////
+ TEMPROIZADOR CON INTERRUPCIONES C
+///////////////////////////////////
+ */ 
+
+#include <xc.h>
+#include <avr/interrupt.h>
+
+/*PROTOTIPOS*/
+void timer0_set(void);
+
+/*VARIABLES*/
+int counter = 0;
+
+void setup(void){
+	// Puertos B y C como salidas
+	DDRB = 0b11111111;
+	DDRC = 0b11111111;
+	// Puerto D como entrada
+	DDRD = 0b00000000;
+	// Se establece el valor de los puertos como 0
+	PORTB = 0;
+	PORTC = 0;
+	PORTD = 0;
+	sei();
+}
+
+int main(void)
+{
+	// Se ejecuta la configuracion de los puertos
+	setup();
+	timer0_set();
+    while(1){
+	}
+}
+
+void timer0_set(void){
+	TCCR0A = 0B00000000;
+	TCCR0B = 0B00000101;
+	TCNT0 = 128;
+	TIMSK0 = 0B00000001;
+}
+
+ISR(TIMER0_OVF_vect, ISR_NAKED){
+	if(counter == 14){
+		counter = 0;
+		if(PINB & (1<<0)){
+			PORTB = 0;
+		}
+		else{
+			PORTB = 1;
+		}
+	}
+	else{
+		counter = counter + 1;
+	}
+	TCNT0 = 128;
+	reti();
+}
